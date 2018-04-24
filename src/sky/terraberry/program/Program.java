b2e0a340843171a1a3a489d3db6b4557111253b3
Pierre-Jean Asmus <pj.asmus@terrasol.com>
2018-04-24 11:41:41 +0200

    Rajout des phrases à cibler à terme en commentaire.

--- src/sky/terraberry/program/Program.java
+++ src/sky/terraberry/program/Program.java
@@ -10,4 +10,7 @@ public class Program
     {
         return null;
     }
+    //Program.at(20).hourAnd(38).minute().do(()->ceci()).andAfter(30).minute().do(()->cela());
+    //Program.at(20).hourAnd(38).minute().do(()->ceci()).every(5).minute().until(21).hourAnd(56).minute();
+    //Program.every(3).second().do(()->ceci());
 }
